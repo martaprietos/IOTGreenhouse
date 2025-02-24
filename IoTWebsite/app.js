5600e1f103465dca8750c6899c3267514d0e6908
MARTA PRIETO SOLOVYOVA - STUDENT <g00431347@atu.ie>
2025-02-24 16:33:05 +0000

    changed the layour of the homepage a bit, used a table to store values and display them

--- IoTWebsite/app.js
+++ IoTWebsite/app.js
@@ -1,6 +1,21 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+
+
+var map = L.map('map').setView([53.276648, -9.011500], 18);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+var marker1 = L.marker([53.276648, -9.011600]).addTo(map);
+var marker2 = L.marker([53.276900, -9.01200]).addTo(map);
+var marker3 = L.marker([53.276648, -9.011200]).addTo(map);
+var marker4 = L.marker([53.276340, -9.01240]).addTo(map);
+
+marker1.bindPopup("<b>Greenhouse 1</b><br>Normal.").openPopup();
+marker2.bindPopup("<b>Greenhouse 2</b><br>Normal.").openPopup();
+marker3.bindPopup("<b>Greenhouse 3</b><br>Normal.").openPopup();
+marker4.bindPopup("<b>Greenhouse 4</b><br>Normal.").openPopup();
